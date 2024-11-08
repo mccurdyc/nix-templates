43bad12578d640291eb430cfd0a31608a430e340
Colton J. McCurdy <mccurdyc22@gmail.com>
2024-11-08 10:08:16 -0500

    update minimal
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- minimal/flake.nix
+++ minimal/flake.nix
@@ -2,7 +2,7 @@
   description = "Repo configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # https://lazamar.co.uk/nix-versions/?package=yarn&version=1.22.19&fullName=yarn-1.22.19&keyName=yarn&revision=336eda0d07dc5e2be1f923990ad9fdb6bc8e28e3&channel=nixpkgs-unstable#instructions
     nixpkgs-foo.url = "https://github.com/NixOS/nixpkgs/archive/336eda0d07dc5e2be1f923990ad9fdb6bc8e28e3.tar.gz";
@@ -12,7 +12,6 @@
 
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      # TODO: currently does nothing
       flake = { };
 
       systems = [
@@ -38,6 +37,22 @@
             inherit system;
             config.allowUnfree = true;
           };
+
+
+          ci_packages = {
+            # Nix
+            nix-fmt = pkgs.nixpkgs-fmt;
+
+            # General
+            inherit (pkgs-unstable) just; # need just >1.33 for working-directory setting
+          };
+
+          packages = (builtins.attrValues ci_packages) ++ [
+            pkgs.statix
+            pkgs.nixpkgs-fmt
+            pkgs-unstable.nil
+            pkgs.hadolint
+          ];
         in
         {
           # This is needed for pkgs-unstable - https://github.com/hercules-ci/flake-parts/discussions/105
@@ -58,25 +73,20 @@
 
                 # Shell
                 shellcheck.enable = true;
-                shfmt.enable = true;
+                shfmt = {
+                  enable = true;
+                  entry = "shfmt --simplify --indent 2";
+                };
               };
             };
           };
 
+          packages = ci_packages;
+
           devShells.default = pkgs.mkShell {
             inherit (self.checks.${system}.pre-commit-check) shellHook;
             buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
-
-            # https://github.com/NixOS/nixpkgs/blob/736142a5ae59df3a7fc5137669271183d8d521fd/doc/build-helpers/special/mkshell.section.md?plain=1#L1
-            # https://github.com/NixOS/nixpkgs/blob/736142a5ae59df3a7fc5137669271183d8d521fd/doc/build-helpers/special/mkshell.section.md?plain=1#L1
-            packages = [
-              pkgs-foo.foo
-
-              pkgs.gnumake
-              pkgs.statix
-              pkgs.nixpkgs-fmt
-              pkgs-unstable.nil
-            ];
+            inherit packages;
           };
         };
     };
