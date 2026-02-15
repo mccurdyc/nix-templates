d791c99d65475de3d59e6115a8a2a4d6be2ce162
Colton J. McCurdy <mccurdyc22@gmail.com>
2026-02-15 15:51:27 -0500

    rust modules
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- rust-modules/flake.nix
+++ rust-modules/flake.nix
@@ -9,6 +9,10 @@
     # - https://github.com/ipetkov/crane
     # - https://github.com/oxalica/rust-overlay
     rust-flake.url = "github:juspay/rust-flake";
+
+    # personal preferences
+    mccurdyc-preferences.url = "path:../modules";
+    # mccurdyc-preferences.url = "github:mccurdyc/nix-templates?dir=modules";
   };
 
   outputs = inputs:
@@ -17,29 +21,21 @@
 
       # imports are core to how flake-parts evaluates flakeModules perSystem
       imports = [
-        # rust-flake proxies rust-overlay and crane modules - https://github.com/juspay/rust-flake/blob/7bb4b549c89bcba9f2c0db3206be282562824655/flake.nix#L22
-        # Use `self'` for outputs (the public interface)
-        # Use `config` for internal module options (the configuration interface)
-        inputs.rust-flake.flakeModules.default
-        inputs.rust-flake.flakeModules.nixpkgs
         inputs.git-hooks.flakeModule
-
-        # Local reusable modules
-        ./nix/modules/rust.nix
-        ./nix/modules/pre-commit.nix
-        ./nix/modules/devshell.nix
+        inputs.mccurdyc-preferences.flakeModules.default
       ];
 
-      perSystem = {
+      perSystem = _: {
         mccurdyc = {
-          rust.enable = true;
-
           pre-commit = {
+            enable = true;
             rust.enable = true;
             just.enable = true;
           };
 
-          devshell.rust.enable = true;
+          devshell = {
+            extraPackages = [ ];
+          };
         };
       };
     };
