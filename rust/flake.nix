484bd8d3de54a82bcdd4f3ca71c21da9fad1bf23
Colton J. McCurdy <mccurdyc22@gmail.com>
2026-05-15 16:41:41 -0400

    [rust] update
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     git-hooks.url = "github:cachix/git-hooks.nix";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -49,41 +48,6 @@
 
           devshell = {
             extraPackages = [ pkgs.rust-analyzer ];
-          };
-
-          dockerfile = {
-            enable = true;
-            extraIgnore = ''
-              target/
-            '';
-            content = ''
-              # syntax=docker/dockerfile:1
-              FROM lukemathwalker/cargo-chef:latest-rust-1.87-alpine AS chef
-              WORKDIR /app
-
-              FROM chef AS planner
-              COPY . .
-              RUN cargo chef prepare --recipe-path recipe.json
-
-              FROM chef AS builder
-              COPY --from=planner /app/recipe.json recipe.json
-              # Build dependencies - this is the caching Docker layer!
-              RUN cargo chef cook --release --recipe-path recipe.json
-              # Build application
-              COPY . .
-              RUN apk add --no-cache just
-              RUN just build
-
-              FROM alpine:3.20 AS runtime
-              RUN apk add --no-cache ca-certificates \
-                  && addgroup -g 1000 app \
-                  && adduser -D -s /bin/sh -u 1000 -G app app
-              WORKDIR /app
-              COPY --from=builder /app/target/release/app /app/app
-              USER app
-              ENTRYPOINT ["/app/app"]
-              CMD ["greet"]
-            '';
           };
         };
       };
